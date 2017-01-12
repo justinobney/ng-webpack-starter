e1e7a8bcab30faa8c4f348c6b1ff1f393259d09b
Justin Obney <justinobney@gmail.com>
2017-01-12 15:41:29 -0600

    feat: sample ui for splitting groups

---
+++ src/app1/screens/home/data.js
@@ -0,0 +1,424 @@
+const data = [
+  {
+    "id": "587793b052a3371c71c26de1",
+    "area": "area-b2"
+  },
+  {
+    "id": "587793b0d5d2d86305bcfacb",
+    "area": "area-b2"
+  },
+  {
+    "id": "587793b056672a5f7f5305fd",
+    "area": "area-b2"
+  },
+  {
+    "id": "587793b07081da9e3282cd59",
+    "area": "area-b2"
+  },
+  {
+    "id": "587793b03391c49265037ffd",
+    "area": "area-b2"
+  },
+  {
+    "id": "587793b043faf1e46f60fe5e",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b0bf1f2b1425f7a415",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b0a2f48a65d58d1876",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b0de4b4fce174bc239",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b0a4a48d5f4065c525",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b0e37f413392e10049",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b00320bc38a8b0177c",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b0f640e33f908356a9",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b0ea7c4e715514c14e",
+    "area": "area-b4"
+  },
+  {
+    "id": "587793b01fc1277875cd4662",
+    "area": "area-b5"
+  },
+  {
+    "id": "587793b01fc1277875cd4662",
+    "area": "area-b5"
+  },
+  {
+    "id": "587793b088a508ab6c4ccd72",
+    "area": "area-b5"
+  },
+  {
+    "id": "587793b09755d0f54a4da0c2",
+    "area": "area-b5"
+  },
+  {
+    "id": "587793b01877bafe12231893",
+    "area": "area-b5"
+  },
+  {
+    "id": "587793b0dca7f99c3ea0e952",
+    "area": "area-b5"
+  },
+  {
+    "id": "587793b035d1cd9aee6d4a0c",
+    "area": "area-b6"
+  },
+  {
+    "id": "587793b059a97a56853da495",
+    "area": "area-b6"
+  },
+  {
+    "id": "587793b0bf5ab7940a84c2df",
+    "area": "area-b6"
+  },
+  {
+    "id": "587793b03fa26394a628ddb8",
+    "area": "area-b6"
+  },
+  {
+    "id": "587793b0ed04c33b39e4bb32",
+    "area": "area-b8"
+  },
+  {
+    "id": "587793b0ed04c33b39e4bb32",
+    "area": "area-b8"
+  },
+  {
+    "id": "587793b0c176927ddd00a668",
+    "area": "area-b8"
+  },
+  {
+    "id": "587793b0ed97f44d0077f4bc",
+    "area": "area-b8"
+  },
+  {
+    "id": "587793b0c9d51af4f4efda8e",
+    "area": "area-c1"
+  },
+  {
+    "id": "587793b0c9d51af4f4efda8e",
+    "area": "area-c1"
+  },
+  {
+    "id": "587793b0ccaa80a0b0f27e6c",
+    "area": "area-c1"
+  },
+  {
+    "id": "587793b0b187504fae01739c",
+    "area": "area-c1"
+  },
+  {
+    "id": "587793b040f409f636182ac8",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0974450d395f2b1c4",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b040f409f636182ac8",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0974450d395f2b1c4",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b040f409f636182ac8",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0974450d395f2b1c4",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b040f409f636182ac8",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0974450d395f2b1c4",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b040f409f636182ac8",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0974450d395f2b1c4",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b040f409f636182ac8",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0974450d395f2b1c4",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b04f35b43ee93e30f0",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b03f27e42e0856a93b",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b09572e20b92dafec9",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b08a19eea668f816c7",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0de7370959b0a6a7c",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0ce655e678a9d2a20",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0878c3046d2aa332a",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0569a30b4312e778a",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0d818690d2a88e9be",
+    "area": "area-c2"
+  },
+  {
+    "id": "587793b0991e5b8508427b47",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b0546014418fa9c063",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b07d2bd917903ef18c",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b0d021c47d72c32a59",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b0ad4a9763cea30eb1",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b03cd992b033213dc9",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b06a15f633bd5f1d2c",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b0a17d3bba8324f5dc",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b05b47939e68ffbd17",
+    "area": "area-c4"
+  },
+  {
+    "id": "587793b069fd9c3f446be4a1",
+    "area": "area-c5"
+  },
+  {
+    "id": "587793b0e97defd7a2b55199",
+    "area": "area-c5"
+  },
+  {
+    "id": "587793b069fd9c3f446be4a1",
+    "area": "area-c5"
+  },
+  {
+    "id": "587793b0e97defd7a2b55199",
+    "area": "area-c5"
+  },
+  {
+    "id": "587793b057cafc2d688d9080",
+    "area": "area-c5"
+  },
+  {
+    "id": "587793b01ed4b1fb450b65c6",
+    "area": "area-c5"
+  },
+  {
+    "id": "587793b0f73b5da5fcceb114",
+    "area": "area-c6"
+  },
+  {
+    "id": "587793b0188250608a6ad082",
+    "area": "area-c6"
+  },
+  {
+    "id": "587793b0f73b5da5fcceb114",
+    "area": "area-c6"
+  },
+  {
+    "id": "587793b0188250608a6ad082",
+    "area": "area-c6"
+  },
+  {
+    "id": "587793b078687c32b501d7bc",
+    "area": "area-c6"
+  },
+  {
+    "id": "587793b0616860c1f6579fe2",
+    "area": "area-c6"
+  },
+  {
+    "id": "587793b0da764cd9d6672bf7",
+    "area": "area-c7"
+  },
+  {
+    "id": "587793b0181c16e7f8fda4a2",
+    "area": "area-c7"
+  },
+  {
+    "id": "587793b00435bc42972f7340",
+    "area": "area-c7"
+  },
+  {
+    "id": "587793b0e5f92f2342408c24",
+    "area": "area-c7"
+  },
+  {
+    "id": "587793b0af1808bbf41f2b46",
+    "area": "area-c8"
+  },
+  {
+    "id": "587793b017f225a73f6d855f",
+    "area": "area-c8"
+  },
+  {
+    "id": "587793b0caf301d6f079976d",
+    "area": "area-c8"
+  },
+  {
+    "id": "587793b0030a277e18cc86e5",
+    "area": "area-c8"
+  },
+  {
+    "id": "587793b03361479457f1df5c",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b03361479457f1df5c",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b0b29105436010269c",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b04aaa9260bdbd1397",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b08cc031fd4c0b245f",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b0cb50114865402106",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b091c5334600a1e492",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b03a78b18ccb6440e0",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b03da3a3f37f745a5a",
+    "area": "area-r4"
+  },
+  {
+    "id": "587793b0ccc989912ee6b0e4",
+    "area": "area-r5"
+  },
+  {
+    "id": "587793b04fded9fa1c475c43",
+    "area": "area-r5"
+  },
+  {
+    "id": "587793b0ee565c198c6a882f",
+    "area": "area-r5"
+  },
+  {
+    "id": "587793b0e10ffc668c1a3fb5",
+    "area": "area-r5"
+  },
+  {
+    "id": "587793b0125d7722bee1af71",
+    "area": "area-r6"
+  },
+  {
+    "id": "587793b0695f8f689df61252",
+    "area": "area-r6"
+  },
+  {
+    "id": "587793b047c3d0a4c0823529",
+    "area": "area-r6"
+  },
+  {
+    "id": "587793b0937b9b56f32d3a5c",
+    "area": "area-r6"
+  },
+  {
+    "id": "587793b0ffba052cf7281b77",
+    "area": "area-r7"
+  },
+  {
+    "id": "587793b02dc64f60b05b0e60",
+    "area": "area-r7"
+  },
+  {
+    "id": "587793b072ba549ccebbf37d",
+    "area": "area-r7"
+  },
+  {
+    "id": "587793b0f3a2afec1c422421",
+    "area": "area-r7"
+  },
+  {
+    "id": "587793b0157c32e9f37b3990",
+    "area": "area-r7"
+  },
+  {
+    "id": "587793b0b857f17d8d3bdf73",
+    "area": "area-r7"
+  }
+];
+
+export default data;
--- src/app1/screens/home/index.js
+++ src/app1/screens/home/index.js
@@ -1,11 +1,75 @@
-import './index.scss';
+import {groupBy, map, sortBy, minBy, maxBy, times} from 'lodash';
 import template from './index.html';
+import data from './data.js';
+import './index.scss';
 
 function controller(){
-  const home = this;
+  const ctrl = this;
 
-  home.$onInit = () => {
-    console.log('home init');
+  ctrl.calculate = calculate;
+
+  ctrl.dataLength = data.length;
+  ctrl.data = sortBy(
+    map(
+      groupBy(data, 'area'),
+      (value, key) => ({key, value, count:value.length })
+    ),
+    x => x.count * -1
+  );
+
+  function calculate(){
+    const users = times(ctrl.userCount, x=> `user: ${x+1}`);
+    const base = data.length / users.length;
+    const result = users.reduce(
+      (acc, user, idx) => {
+        const halfWay = users.length / 2;
+        const val = idx + 1 >= halfWay ? Math.ceil(base) : Math.floor(base);
+        const idealSize = users.length - 1 === idx ? acc.remaining : val;
+        acc.remaining -= idealSize;
+
+        acc.users.push({
+          user,
+          idealSize: idealSize,
+          itemCount: 0,
+          items: [],
+        });
+        return acc;
+      },
+      {users:[], remaining: data.length}
+    );
+
+    const copy = [...ctrl.data];
+
+    for (var i = 0; i < result.users.length; i++) {
+      const {key, count} = copy.shift();
+      const user = result.users[i];
+      user.items = user.items.concat([{key, count}]);
+      user.itemCount += count;
+    }
+
+    while (copy.length) {
+      const slacker = minBy(result.users, 'itemCount');
+      const {key, count} = copy.shift();
+      slacker.items = slacker.items.concat([{key, count}]);
+      slacker.itemCount += count;
+    }
+
+    const largest = maxBy(result.users, 'itemCount').itemCount;
+    const normalized = sortBy(
+      result.users.map(x => {
+        x.percent = ((x.itemCount / largest) * 100).toFixed(2);
+        x.percentStyle = `${x.percent}%`;
+        return x;
+      }),
+      x => -1 * x.itemCount
+    );
+
+    result.users = normalized;
+    ctrl.result = result;
+  }
+
+  function findSlacker(result){
+    const slacker = minBy(result, 'itemCount');
   }
 }
 
